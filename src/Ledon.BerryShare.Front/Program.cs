07f8d7fdf352736f0d4d4f82f0abb997cf2d2f61
ledon <powertle@outlook.com>
2025-08-14 17:23:38 +0800

    更新 HttpClient 基地址为动态获取，确保与主机环境一致

--- src/Ledon.BerryShare.Front/Program.cs
+++ src/Ledon.BerryShare.Front/Program.cs
@@ -9,7 +9,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5252/") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddScoped<ITokenProvider, TokenProvider>();
 builder.Services.AddScoped<ApiService>();
